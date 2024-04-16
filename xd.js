4824675b4116959596aeae35559cce19445c8f02
Roman Izaguirre <rizaguirre976@gmail.com>
2024-04-16 17:18:16 -0300

    first commmit

---
+++ xd.js
@@ -0,0 +1,8 @@
+function sumarNumeros(n) {
+    let suma = 0;
+    for (let i = 1; i <= n; i+) 
+        suma = i;
+    }
+}
+let resultado = sumarNumeros(5);
+console.log ("la suma de los numeros del 1 al 5 es: " + resultado);
